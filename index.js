84a6435272a3f00965f4b37b4b7b4227ca83d85a
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-04 10:47:32 -0400

    Added tile layer to map

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ function shopsNearMe() {
 
 function displayPosition(position) {
     var my_map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
+    L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+    {
+      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+      maxZoom: 17,
+      minZoom: 9
+    }).addTo(my_map);
 }
