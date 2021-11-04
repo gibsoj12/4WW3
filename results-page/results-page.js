28bad0adccbbfee9cdcd0b627e93c15618c27840
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2021-11-04 17:09:41 -0400

    text should now show.

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -1,4 +1,3 @@
-var x = document.getElementById("map_text");
 /*
 function displayMyLocation() {
   if (navigator.geolocation) {
@@ -9,17 +8,19 @@ function displayMyLocation() {
 } */
 
 function displayMyLocation() {
-    
-    x.innerHTML = "Latitude: " + "43.262114" +
-    "<br>Longitude: " + "-79.905834";
 
-    var my_map = L.map('map').setView([43.262114, -79.905834], 14);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
-    }).addTo(my_map);
+  var x = document.getElementById("map_text");
+    
+  x.innerHTML = "Latitude: " + "43.262114" +
+  "<br>Longitude: " + "-79.905834";
+
+  var my_map = L.map('map').setView([43.262114, -79.905834], 14);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
+  }).addTo(my_map);
 }
