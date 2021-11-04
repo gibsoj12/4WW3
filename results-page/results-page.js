1d7d10fa9c02bb6722cdab011a4a9254946a09a9
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-04 11:21:37 -0400

    error checking now

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -1,9 +1,17 @@
-function displayMyLocation() {
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 
 function displayPosition(position) {
-    document.writeln(position.coords.latitude);
+    
+    x.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
+
     var my_map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
