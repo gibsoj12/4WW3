8d8a23e7d172ee31f5cdf8ce66fec9a1834090c5
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-04 16:22:25 -0400

    changed to hard-coded lat long

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -1,18 +1,18 @@
-var x = document.getElementById("demo");
+/* var x = document.getElementById("demo");
 function displayMyLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayPosition);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
-}
+} */
 
-function displayPosition(position) {
+function displayMyLocation() {
     
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+    x.innerHTML = "Latitude: " + "43.262114" +
+    "<br>Longitude: " + "-79.905834"
 
-    var my_map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
+    var my_map = L.map('map').setView([43.262114, -79.905834], 14);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
