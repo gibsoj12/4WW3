7a494b1e89a52aaa9a32a56643988ae4baa4f682
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2021-11-04 17:08:04 -0400

    text should now show.

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -1,4 +1,5 @@
-/*var x = document.getElementById("map");
+var x = document.getElementById("map_text");
+/*
 function displayMyLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayPosition);
@@ -9,16 +10,16 @@ function displayMyLocation() {
 
 function displayMyLocation() {
     
-    /* x.innerHTML = "Latitude: " + "43.262114" +
-    "<br>Longitude: " + "-79.905834"; */
+    x.innerHTML = "Latitude: " + "43.262114" +
+    "<br>Longitude: " + "-79.905834";
 
     var my_map = L.map('map').setView([43.262114, -79.905834], 14);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bGVyZ3E1NmRmMjJ1cTFkMDg3NXM4MyJ9.qwcU5DNE5EX6jzAZk5I_qA'
-}).addTo(my_map);
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
+    }).addTo(my_map);
 }
