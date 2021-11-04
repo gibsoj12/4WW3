cc5fc251b7578c6f49f3b80968fa86d13dc8d8de
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-04 11:03:37 -0400

    now onLoad shows location.

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 function shopsNearMe() {
     window.location = "./results-page/results-page.html";
-    navigator.geolocation.getCurrentPosition(displayPosition);
-}
-
-function displayPosition(position) {
-    
-    var my_map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
-    L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-    {
-      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-      maxZoom: 17,
-      minZoom: 9
-    }).addTo(my_map);
 }
