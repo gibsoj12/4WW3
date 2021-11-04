1f2f085de188e7312d149f1cdf006158a4637527
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2021-11-04 17:13:37 -0400

    hopefully this works now

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -14,7 +14,7 @@ function displayMyLocation() {
   x.innerHTML = "Latitude: " + "43.262114" +
   "<br>Longitude: " + "-79.905834";
 
-  var my_map = L.map('map').setView([43.262114, -79.905834], 14);
+  var my_map = L.map('my_map').setView([43.262114, -79.905834], 14);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
