22b3f617f84a66cdd142ccda9d6ca4e325f08900
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-04 11:14:25 -0400

    adding tile layer.

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -5,10 +5,12 @@ function displayMyLocation() {
 function displayPosition(position) {
     
     var my_map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 14);
-    L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-    {
-      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-      maxZoom: 17,
-      minZoom: 9
-    }).addTo(my_map);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
+}).addTo(my_map);
 }
