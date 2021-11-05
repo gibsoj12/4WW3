8438facbb1c892ecd03ff46c6160dce114457e60
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-05 10:54:27 -0400

    working locally now.

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -14,13 +14,18 @@ function displayMyLocation() {
   x.innerHTML = "Latitude: " + "43.262114" +
   "<br>Longitude: " + "-79.905834";
 
-  var my_map = L.map('my_map').setView([43.262114, -79.905834], 14);
+  var my_map = L.map('my_map').setView([43.262114, -79.905834], 16);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: 20,
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
   }).addTo(my_map);
+
+  L.marker([43.262114, -79.905834]).addTo(my_map)
+    .bindPopup('Paisleys Coffeehouse and Eatery.<br><a href="../about-page/about-page.html">Paisleys review</a>')
+    .openPopup();
+
 }
