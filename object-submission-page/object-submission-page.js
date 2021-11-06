48d47993640071f6fce5855be76104c91f261960
Jeff Gibson <gibsoj12@mcmaster.ca>
2021-11-06 11:43:15 -0400

    object submission page map stuff done.

---
+++ object-submission-page/object-submission-page.js
@@ -0,0 +1,31 @@
+var x = document.getElementById("map_text");
+
+function displayMyLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(displayPosition);
+    } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+
+function displayPosition(position) {
+
+    var name = document.getElementById("name").value;
+
+    x.innterHTML = "Latitude: " + position.coordinates.latitude +
+      "<br>Longitude: " + position.coordinates.longitude;
+
+      var my_map = L.map('my_map').setView([position.coordinates.latitude, position.coordinates.longitude], 16);
+      L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 20,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
+      }).addTo(my_map);
+    
+      L.marker([position.coordinates.latitude, position.coordinates.longitude]).addTo(my_map)
+        .bindPopup(name)
+        .openPopup();
+}
