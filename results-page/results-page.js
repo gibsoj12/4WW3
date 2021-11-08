c3ec2f8d2c0163f12c1b928a29a9a70bd1650d4e
Jessica <jessica.deleeuw2@gmail.com>
2021-11-07 21:18:18 -0500

    Added live map to results pge

--- results-page/results-page.js
+++ results-page/results-page.js
@@ -1,20 +1,19 @@
-/*
 function displayMyLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(displayPosition);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
-} */
+}
 
-function displayMyLocation() {
+function displayPosition(position) {
 
   var x = document.getElementById("map_text");
     
-  x.innerHTML = "Latitude: " + "43.262114" +
-  "<br>Longitude: " + "-79.905834";
+  x.innerHTML = "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude;
 
-  var my_map = L.map('my_map').setView([43.262114, -79.905834], 16);
+  var my_map = L.map('my_map').setView([position.coords.latitude, position.coords.longitude], 16);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 20,
