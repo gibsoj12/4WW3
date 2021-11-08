ce4ee2d909a5baa97473fb7f8e69426a572f72ba
Jessica <jessica.deleeuw2@gmail.com>
2021-11-07 21:11:01 -0500

    Updating call to get coordinates

--- object-submission-page/object-submission-page.js
+++ object-submission-page/object-submission-page.js
@@ -12,10 +12,10 @@ function displayPosition(position) {
 
     var name = document.getElementById("name").value;
 
-    x.innterHTML = "Latitude: " + position.coordinates.latitude +
-      "<br>Longitude: " + position.coordinates.longitude;
+    x.innterHTML = "Latitude: " + position.coords.latitude +
+      "<br>Longitude: " + position.coords.longitude;
 
-      var my_map = L.map('my_map').setView([position.coordinates.latitude, position.coordinates.longitude], 16);
+      var my_map = L.map('my_map').setView([position.coords.latitude, position.coords.longitude], 16);
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 20,
@@ -25,7 +25,7 @@ function displayPosition(position) {
         accessToken: 'pk.eyJ1IjoiZ2lic29qMTIiLCJhIjoiY2t2bDM3Z2Z6MzBnaTJvbnl6YjYyYXB5NiJ9.oUYYVzj-s5a25lh415HLQQ'
       }).addTo(my_map);
     
-      L.marker([position.coordinates.latitude, position.coordinates.longitude]).addTo(my_map)
+      L.marker([position.coords.latitude, position.coords.longitude]).addTo(my_map)
         .bindPopup(name)
         .openPopup();
 }
