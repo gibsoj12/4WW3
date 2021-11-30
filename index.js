440f7f7856a6f42fe47e193c10a1c19909eff9a5
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2021-11-30 15:29:47 -0500

    Logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function shopsNearMe() {
 
 function searchName() {
     const shopName = document.getElementById('name-search').value;
+    console.log(shopName);
     sessionStorage.setItem("shopName", shopName);
     window.location = "./results-page/results-page.html";
 }
