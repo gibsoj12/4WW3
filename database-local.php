31f97e971866cc6ac06b596026d761eab1e2f90b
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2021-12-08 15:11:25 -0500

    Added file for local dev

---
+++ database-local.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+    private $host = "localhost";
+    private $db_name = "coffee_house";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+
+    public function getConnection() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+
+        if($this->conn->connect_error) {
+            $response['status'] = '0';
+            $response['message'] = $this->conn->connect_error;
+            return $response;
+        } else {
+            $response['status'] = '1';
+            $response['connection'] = $this->conn;
+            return $response;
+        }
+    }
+}
+
+?>
